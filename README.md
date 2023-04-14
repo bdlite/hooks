bd4de76c7e30cad6d990386619d5b0d0070e1384
Shirley89 <yexinxie@163.com>
2023-04-14 15:06:09 +0000

    chore: 更新README

--- README.md
+++ README.md
@@ -9,3 +9,30 @@ Bdlite团队自用hooks，有想用的小伙伴欢迎联系我们 yexinxie@163.c
 协议开放
 
 
+## 安装步骤
+
+- 克隆到本地
+
+    `
+    git clone https://github.com/bdlite/hooks.git
+    `
+
+- 安装
+
+    `
+    yarn install
+    `
+
+- 开始单元测试
+
+    - 可以直接 run (指向的是es目录)
+
+        `
+        npm run test
+        `
+
+    - 也可以模拟发布前
+
+        `
+        npm run prepublishOnly
+        `
--- es/useSearch.js
+++ es/useSearch.js
@@ -1,23 +1,27 @@
-import { useCallback } from 'react'
-import queryString from 'query-string'
-
+import _defineProperty from "@babel/runtime/helpers/defineProperty";
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+import { useCallback } from 'react';
+import queryString from 'query-string';
 export function useSearch() {
-  const searchList = [] // 同一组件连续调用的缓冲区
+  var searchList = []; // 同一组件连续调用的缓冲区
 
-  const getSearch = useCallback(() => queryString.parse(window.location.search), [ window.location.search ])
-
-  const setSearch = useCallback((key, value = null) => {
-    const search = getSearch()
-
-    if (search[key] === `${value}` || typeof key !=='string') return
-
-    searchList.push({ [key]: value })
-
-    const nextSearchData = { ...search, ...searchList.reduce((before, current) => ({ ...before, ...current }), {}) }
-    const nextSearch = queryString.stringify(nextSearchData, { skipNull: true })
-
-    window.history.replaceState(queryString.parse(nextSearch), '', `?${nextSearch}`)
-  }, [ getSearch, window.history ])
-
-  return { getSearch, setSearch }
+  var getSearch = useCallback(() => queryString.parse(window.location.search), [window.location.search]);
+  var setSearch = useCallback(function (key) {
+    var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+    var search = getSearch();
+    if (search[key] === "".concat(value) || typeof key !== 'string') return;
+    searchList.push({
+      [key]: value
+    });
+    var nextSearchData = _objectSpread(_objectSpread({}, search), searchList.reduce((before, current) => _objectSpread(_objectSpread({}, before), current), {}));
+    var nextSearch = queryString.stringify(nextSearchData, {
+      skipNull: true
+    });
+    window.history.replaceState(queryString.parse(nextSearch), '', "?".concat(nextSearch));
+  }, [getSearch, window.history]);
+  return {
+    getSearch,
+    setSearch
+  };
 }
