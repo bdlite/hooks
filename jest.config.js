39d2830699e3a469806375762e843aff3f7a5825
Shirley89 <yexinxie@163.com>
2023-04-19 08:40:46 +0000

    chore: 更新jest配置及相应的修改

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,39 @@
 module.exports = {
-    testEnvironment: 'jsdom', // 使用 jsdom 作为测试环境
-    // setupFilesAfterEnv: [
-    //   '<rootDir>/setup.js'
-    // ],
-    setupFilesAfterEnv: ['<rootDir>/setup.js', '@testing-library/jest-dom'],
-    moduleDirectories: ['node_modules', 'es', 'src'], // 指定搜索模块的目录
-    moduleFileExtensions: [ // 指定解析模块时要查找的文件扩展名
-      'js',
-      'jsx',
-      'ts',
-      'tsx',
-      'json',
-      'node'
-    ],
-    moduleNameMapper: { // 将特定的文件扩展名映射到模拟文件
-      '^src/(.*)$': '<rootDir>/src/$1', // 将 src 目录下的文件映射到根目录下
-      '^es/(.*)$': '<rootDir>/es/$1', // 将 es 目录下的文件映射到根目录下
-    },
-    coveragePathIgnorePatterns: [ // 指定计算代码覆盖率时要忽略的目录
-      "/node_modules/",
-    ],
-    // extensionsToTreatAsEsm: ['.ts'], // 将 TypeScript 文件视为 ES 模块
-    transform: { // 指定要使用的转换器以处理不同类型的文件
-      '^.+\\.ts$': 'ts-jest',
-      '^.+\\.tsx$': 'ts-jest',
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    // transformIgnorePatterns: [
-    //   '/node_modules/(?!query-string)/'
-    // ],
-    // globals: { // 指定在测试期间要使用的全局变量
-    //   'window': true,
-    // },
-    testMatch: [ // 指定要运行的测试文件
-      '<rootDir>/\_\_tests\_\_/\*\*/\*.test.js?(x)'
-    ],
-  };
+  // 设置测试环境为jsdom
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup.js', '@testing-library/jest-dom'],
+  // 设置模块目录
+  moduleDirectories: ['node_modules', 'es', 'src'],
+  // 设置模块文件扩展名
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node'
+  ],
+  // 设置模块名称映射
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^es/(.*)$': '<rootDir>/es/$1',
+  },
+  // 设置忽略测试覆盖率的路径
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+  ],
+  // 设置转换器
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  // 设置全局变量
+  // globals: {
+  //   'window': true,
+  // },
+  // 设置测试文件匹配规则
+  testMatch: [
+    '<rootDir>/\_\_tests\_\_/\*\*/\*.test.js?(x)'
+  ],
+};
